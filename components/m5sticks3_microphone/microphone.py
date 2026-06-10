4b1285be13ba61506576fc782205ca775b7a601a
Aguacatec <106380506+TitoTB@users.noreply.github.com>
2026-06-10 07:02:57 +0200

    Beta

---
+++ components/m5sticks3_microphone/microphone.py
@@ -0,0 +1,40 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import audio, microphone
+from esphome.const import CONF_ID, CONF_SAMPLE_RATE
+
+DEPENDENCIES = ["esp32"]
+
+m5sticks3_microphone_ns = cg.esphome_ns.namespace("m5sticks3_microphone")
+M5StickS3Microphone = m5sticks3_microphone_ns.class_(
+    "M5StickS3Microphone", microphone.Microphone, cg.Component
+)
+
+CONF_BUFFER_DURATION = "buffer_duration"
+
+CONFIG_SCHEMA = cv.All(
+    microphone.MICROPHONE_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(M5StickS3Microphone),
+            cv.Optional(CONF_SAMPLE_RATE, default=16000): cv.int_range(min=8000, max=48000),
+            cv.Optional(CONF_BUFFER_DURATION, default="20ms"): cv.positive_time_period_milliseconds,
+        }
+    ).extend(cv.COMPONENT_SCHEMA),
+    audio.set_stream_limits(
+        min_bits_per_sample=16,
+        max_bits_per_sample=16,
+        min_channels=1,
+        max_channels=1,
+        min_sample_rate=16000,
+        max_sample_rate=16000,
+    ),
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await microphone.register_microphone(var, config)
+
+    cg.add(var.set_sample_rate(config[CONF_SAMPLE_RATE]))
+    cg.add(var.set_buffer_duration(config[CONF_BUFFER_DURATION]))
